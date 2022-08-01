f2fb4643993e8bcaa561bd654b8f3796ae7f5528
bryan <bryan@binauraldev.com>
2022-08-01 09:50:30 -0400

    Issue: Error confirmar quant

--- custom/logan_stock/models/stock_quant.py
+++ custom/logan_stock/models/stock_quant.py
@@ -1,14 +1,21 @@
 from odoo import models, fields, api
-from odoo.exceptions import UserError, ValidationError
 
 
 class StockQuantExtend(models.Model):
+
     _inherit = 'stock.quant'
 
     reason = fields.Char(string='Motivo de modificación', required=True)
 
-    @api.constrains('reason')
-    def _check_reason(self):
-        for record in self:
-            if not record.reason:
-                raise ValidationError('El motivo de modificación es requerido')
+
+    """
+    Al 30 de julio del 2022 el equipo de desarrollo se dio cuenta que para poder crear un campo modificable dentro del
+    stock.quant es necesario agregarlo en _get_inventory_fields_create (esto no pasa en odoo14)
+    """
+    @api.model
+    def _get_inventory_fields_create(self):
+        """ Returns a list of fields user can edit when he want to create a quant in `inventory_mode`.
+        """
+        res = super()._get_inventory_fields_create()
+        res.append('reason')
+        return res
